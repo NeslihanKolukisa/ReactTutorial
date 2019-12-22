57774c7e83b3ec8f891c34ad5c10e46aebbc8e6b
neslihan262 <262neslihan@gmail.com>
2019-12-22 14:45:46 +0300

    Functional components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
 
 
 function App() {
     return (
     <div className="container">
-        <h4 style={{color:"blue",fontSize:"30px"}}>App Component</h4>
-        <h4 className="header">App Component</h4>
-        <h4>Neslihan</h4>
+        <Navbar/>
     </div>
   );
 }

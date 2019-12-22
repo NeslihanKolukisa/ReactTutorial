dbed0adb1166f097528cde968057e39f62ca8e3b
neslihan262 <262neslihan@gmail.com>
2019-12-22 15:00:16 +0300

    Props using and destructing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
 
+import User from "./components/User";
+
 
 function App() {
     return (
     <div className="container">
-        <Navbar/>
+        <Navbar title="User App 2" />
+        <hr />
+        <User
+            name = "Neslihan KOLUKISA"
+            salary = "0 TL"
+            department = "KKST"
+        />
+
+        <User
+            name = "Oğuz Artıran"
+            salary = "60000 TL"
+            department = "Bilişim"
+        />
     </div>
   );
 }
